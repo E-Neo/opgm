2f839735e8045a2e1b26ded5dd88d2b652e432bb
E-Neo <e-neo@qq.com>
2020-10-21 21:37:52 +0800

    fix bugs in match_characteristics.rs

--- src/executor/match_characteristics.rs
+++ src/executor/match_characteristics.rs
@@ -7,7 +7,7 @@ use crate::{
     types::{PosLen, SuperRowHeader, VId, VIdPos, VLabel},
 };
 use std::cmp::Ordering;
-use std::collections::{BTreeMap, HashMap, HashSet};
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 use std::mem::size_of;
 
 /// Match the stars by matching the `characteristic`s.
@@ -23,11 +23,10 @@ pub fn match_characteristics(
     super_row_mms: &mut [MemoryManager],
     index_mms: &mut [MemoryManager],
 ) {
-    let ids: Vec<usize> = infos.iter().enumerate().map(|(id, _)| id).collect();
     let (vertices_len, vertices) = data_graph.vertices(vlabel);
-    initialize_results(&ids, super_row_mms, index_mms, vertices_len);
-    let sr_pos_idx_poses = scan_data_vertices(vertices, &ids, infos, super_row_mms, index_mms);
-    finish_results(&ids, infos, super_row_mms, index_mms, &sr_pos_idx_poses);
+    initialize_results(infos, super_row_mms, index_mms, vertices_len);
+    let sr_pos_idx_poses = scan_data_vertices(vertices, infos, super_row_mms, index_mms);
+    finish_results(infos, super_row_mms, index_mms, &sr_pos_idx_poses);
 }
 
 /// Allocate space for `SuperRowHeader` and the indices.
@@ -38,31 +37,23 @@ pub fn match_characteristics(
 /// by [`vertices()`](../data_graph/struct.DataGraph.html#method.vertices),
 /// so it could be allocate once for all.
 fn initialize_results(
-    ids: &[usize],
+    infos: &[CharacteristicInfo],
     super_row_mms: &mut [MemoryManager],
     index_mms: &mut [MemoryManager],
     vertices_len: usize,
 ) {
-    ids.iter().for_each(|&id| unsafe {
-        super_row_mms
-            .get_unchecked_mut(id)
-            .resize(size_of::<SuperRowHeader>());
-        index_mms
-            .get_unchecked_mut(id)
-            .resize(vertices_len * size_of::<VIdPos>());
+    infos.iter().map(|info| info.id()).for_each(|id| {
+        super_row_mms[id].resize(size_of::<SuperRowHeader>());
+        index_mms[id].resize(vertices_len * size_of::<VIdPos>());
     });
 }
 
-/// Returns `(sr_pos, idx_pos)` for each `characteristic` in `characteristics`.
+/// Returns `(sr_pos, idx_pos)` for `super_row_mms`.
 ///
-/// Please note that the result of this function has the same length as `infos`,
-/// `super_row_mms` and `index_mms`.
-/// That is to say, we use the vector as an ad-hoc hash table and the elements
-/// not in `ids` don't have any meanings.
-/// Though it is a little bit dirty, it works fine as an internal interface.
+/// Please note that the result of this function has the same length as `index_mms`,
+/// but only the `id`s correspond to `infos` are valid.
 fn scan_data_vertices<'a, VS>(
     vertices: VS,
-    ids: &[usize],
     infos: &[CharacteristicInfo],
     super_row_mms: &mut [MemoryManager],
     index_mms: &mut [MemoryManager],
@@ -70,12 +61,13 @@ fn scan_data_vertices<'a, VS>(
 where
     VS: IntoIterator<Item = DataVertex<'a>>,
 {
-    let mut sr_pos_idx_poses = vec![(size_of::<SuperRowHeader>(), 0); infos.len()];
+    let mut sr_pos_idx_poses = vec![(size_of::<SuperRowHeader>(), 0); index_mms.len()];
+    let nlabel_offsets = group_characteristic_by_nlabel(infos);
     for vertex in vertices {
         match_data_vertex(
             &vertex,
-            ids,
             infos,
+            &nlabel_offsets,
             super_row_mms,
             index_mms,
             &mut sr_pos_idx_poses,
@@ -84,31 +76,55 @@ where
     sr_pos_idx_poses
 }
 
+/// Returns the offset of `info` in `infos` groupped by nlabel.
+fn group_characteristic_by_nlabel(infos: &[CharacteristicInfo]) -> Vec<(VLabel, Vec<usize>)> {
+    let mut nlabel_offsets: BTreeMap<VLabel, BTreeSet<usize>> = BTreeMap::new();
+    infos.iter().enumerate().for_each(|(offset, info)| {
+        info.characteristic()
+            .infos()
+            .iter()
+            .map(|ninfo| ninfo.vlabel())
+            .for_each(|nlabel| {
+                nlabel_offsets.entry(nlabel).or_default().insert(offset);
+            });
+    });
+    nlabel_offsets
+        .into_iter()
+        .map(|(nlabel, offsets)| (nlabel, offsets.into_iter().collect()))
+        .collect()
+}
+
 /// Scan the `vertex` and its neighbors to match characteristics.
 ///
 /// We first select characteristic candidates by early filters,
 /// i.e., root's vertex constraint and neighbors' labels.
-/// And then group the candidates by neighbors' label such that the neighbors could be scanned
-/// sequentially only once.
+/// And then scan the neighbors of `vertex` at most once to match the characteristics.
 fn match_data_vertex(
     vertex: &DataVertex,
-    ids: &[usize],
     infos: &[CharacteristicInfo],
+    nlabel_offsets: &[(VLabel, Vec<usize>)],
     super_row_mms: &mut [MemoryManager],
     index_mms: &mut [MemoryManager],
     sr_pos_idx_poses: &mut [(usize, usize)],
 ) {
-    let id_sr_pos_eqv_poses = get_characteristic_candidates(
-        vertex,
-        ids,
-        infos,
-        super_row_mms,
-        sr_pos_idx_poses,
-        index_mms,
-    );
-    let nlabel_ids =
-        group_characteristics_by_nlabel(id_sr_pos_eqv_poses.keys().map(|&id| id), infos);
-    let (mut left_iter, mut right_iter) = (vertex.vlabels(), nlabel_ids.iter());
+    let offset_sr_pos_eqv_poses =
+        get_characteristic_candidates(vertex, infos, super_row_mms, index_mms, sr_pos_idx_poses);
+    let nlabel_offsets: Vec<_> = nlabel_offsets
+        .iter()
+        .filter_map(|(nlabel, offsets)| {
+            let offsets: Vec<usize> = offsets
+                .iter()
+                .filter(|&&offset| offset_sr_pos_eqv_poses.contains_key(&offset))
+                .map(|&x| x)
+                .collect();
+            if offsets.is_empty() {
+                None
+            } else {
+                Some((*nlabel, offsets))
+            }
+        })
+        .collect();
+    let (mut left_iter, mut right_iter) = (vertex.vlabels(), nlabel_offsets.iter());
     let (mut left, mut right) = (left_iter.next(), right_iter.next());
     while let (Some(x), Some(y)) = (left, right) {
         match x.0.cmp(&y.0) {
@@ -121,7 +137,7 @@ fn match_data_vertex(
                     y.1.as_slice(),
                     infos,
                     super_row_mms,
-                    &id_sr_pos_eqv_poses,
+                    &offset_sr_pos_eqv_poses,
                 );
                 left = left_iter.next();
                 right = right_iter.next();
@@ -136,43 +152,43 @@ fn match_neighbors<'a, N>(
     vertex: &DataVertex,
     nlabel: VLabel,
     neighbors: N,
-    ids: &[usize],
+    offsets: &[usize],
     infos: &[CharacteristicInfo],
     super_row_mms: &mut [MemoryManager],
-    id_sr_pos_eqv_poses: &HashMap<usize, (usize, Vec<usize>)>,
+    offset_sr_pos_eqv_poses: &HashMap<usize, (usize, Vec<usize>)>,
 ) where
     N: IntoIterator<Item = DataNeighbor<'a>>,
 {
-    let id_sr_pos_eqv_ninfo_poses =
-        get_id_sr_pos_eqv_info_poses(nlabel, ids, infos, id_sr_pos_eqv_poses);
-    let mut id_lens: Vec<Vec<usize>> = id_sr_pos_eqv_ninfo_poses
+    let offset_sr_pos_eqv_ninfo_poses =
+        get_offset_sr_pos_eqv_info_poses(nlabel, offsets, infos, offset_sr_pos_eqv_poses);
+    let mut offset_lens: Vec<Vec<usize>> = offset_sr_pos_eqv_ninfo_poses
         .iter()
         .map(|(_, (_, ninfo_poses))| ninfo_poses.iter().map(|_| 0).collect())
         .collect();
     for neighbor in neighbors {
-        for (i, (id, (_, eqv_ninfo_poses))) in id_sr_pos_eqv_ninfo_poses.iter().enumerate() {
+        for (i, (offset, (_, eqv_ninfo_poses))) in offset_sr_pos_eqv_ninfo_poses.iter().enumerate()
+        {
             for (j, (_, ninfo, pos)) in eqv_ninfo_poses.iter().enumerate() {
                 if check_neighbor_constraints(vertex, &neighbor, ninfo)
                     && check_neighbor_degrees(&neighbor, ninfo)
                     && check_neighbor_edges(&neighbor, ninfo)
                 {
                     write_vid(
-                        unsafe { super_row_mms.get_unchecked_mut(*id) },
-                        pos + unsafe { id_lens.get_unchecked(i).get_unchecked(j) }
-                            * size_of::<VId>(),
+                        &mut super_row_mms[infos[*offset].id()],
+                        pos + offset_lens[i][j] * size_of::<VId>(),
                         neighbor.id(),
                     );
-                    *unsafe { id_lens.get_unchecked_mut(i).get_unchecked_mut(j) } += 1;
+                    offset_lens[i][j] += 1;
                 }
             }
         }
     }
-    for ((id, (sr_pos, eqv_ninfo_poses)), lens) in
-        id_sr_pos_eqv_ninfo_poses.iter().zip(id_lens.iter())
+    for ((offset, (sr_pos, eqv_ninfo_poses)), lens) in
+        offset_sr_pos_eqv_ninfo_poses.iter().zip(offset_lens.iter())
     {
         for (&(eqv, _, pos), &len) in eqv_ninfo_poses.iter().zip(lens) {
             write_pos_len(
-                unsafe { super_row_mms.get_unchecked_mut(*id) },
+                &mut super_row_mms[infos[*offset].id()],
                 *sr_pos,
                 eqv,
                 pos,
@@ -183,25 +199,26 @@ fn match_neighbors<'a, N>(
 }
 
 /// Get necessary static information to match neighbors.
-fn get_id_sr_pos_eqv_info_poses<'a, 'b>(
+fn get_offset_sr_pos_eqv_info_poses<'a, 'b>(
     nlabel: VLabel,
-    ids: &[usize],
+    offsets: &[usize],
     infos: &[CharacteristicInfo<'a, 'b>],
     id_sr_pos_eqv_poses: &HashMap<usize, (usize, Vec<usize>)>,
 ) -> Vec<(usize, (usize, Vec<(usize, &'a NeighborInfo<'b>, usize)>))> {
-    ids.iter()
-        .map(|&id| {
-            let (sr_pos, eqv_poses) = id_sr_pos_eqv_poses.get(&id).unwrap();
+    offsets
+        .iter()
+        .map(|&offset| {
+            let (sr_pos, eqv_poses) = id_sr_pos_eqv_poses.get(&offset).unwrap();
             (
-                id,
+                offset,
                 (
                     *sr_pos,
-                    unsafe { infos.get_unchecked(id) }
+                    infos[offset]
                         .nlabel_ninfo_eqv()
                         .get(&nlabel)
                         .unwrap()
                         .iter()
-                        .map(|&(ninfo, eqv)| (eqv, ninfo, *unsafe { eqv_poses.get_unchecked(eqv) }))
+                        .map(|&(ninfo, eqv)| (eqv, ninfo, eqv_poses[eqv]))
                         .collect(),
                 ),
             )
@@ -264,69 +281,45 @@ fn check_neighbor_edges(neighbor: &DataNeighbor, info: &NeighborInfo) -> bool {
 
 /// Gets characteristic candidates and allocates space.
 ///
-/// Returns the candidate `id` together with original `sr_pos`
+/// Returns the candidate offset in `infos` together with original `sr_pos`
 /// and the allocated starting position for each equivalence class in the `SuperRow`.
 /// The index is also updated by this function.
 fn get_characteristic_candidates(
     vertex: &DataVertex,
-    ids: &[usize],
     infos: &[CharacteristicInfo],
     super_row_mms: &mut [MemoryManager],
-    sr_pos_idx_poses: &mut [(usize, usize)],
     index_mms: &mut [MemoryManager],
+    sr_pos_idx_poses: &mut [(usize, usize)],
 ) -> HashMap<usize, (usize, Vec<usize>)> {
-    ids.iter()
-        .filter_map(|&id| {
-            unsafe { infos.get_unchecked(id) }
-                .characteristic()
+    infos
+        .iter()
+        .enumerate()
+        .filter_map(|(offset, info)| {
+            info.characteristic()
                 .root_constraint()
-                .map_or(
-                    Some(id),
-                    |vc| if vc.f()(vertex.id()) { Some(id) } else { None },
-                )
+                .map_or(Some((offset, info)), |vc| {
+                    if vc.f()(vertex.id()) {
+                        Some((offset, info))
+                    } else {
+                        None
+                    }
+                })
         })
-        .filter_map(|id| {
-            unsafe {
-                allocate(
-                    super_row_mms.get_unchecked_mut(id),
-                    sr_pos_idx_poses.get_unchecked(id).0,
-                    vertex,
-                    infos.get_unchecked(id),
-                )
-            }
+        .filter_map(|(offset, info)| {
+            allocate(
+                &mut super_row_mms[info.id()],
+                sr_pos_idx_poses[info.id()].0,
+                vertex,
+                info,
+            )
             .map(|(new_sr_pos, eqv_poses)| {
-                let sr_pos_idx_pos = unsafe { sr_pos_idx_poses.get_unchecked_mut(id) };
-                let sr_pos = sr_pos_idx_pos.0;
-                write_index(
-                    unsafe { index_mms.get_unchecked_mut(id) },
-                    sr_pos_idx_pos.1,
-                    vertex.id(),
-                    sr_pos_idx_pos.0,
-                );
-                sr_pos_idx_pos.0 = new_sr_pos;
-                sr_pos_idx_pos.1 += size_of::<VIdPos>();
-                (id, (sr_pos, eqv_poses))
+                let (sr_pos, idx_pos) = sr_pos_idx_poses[info.id()];
+                write_index(&mut index_mms[info.id()], idx_pos, vertex.id(), sr_pos);
+                sr_pos_idx_poses[info.id()] = (new_sr_pos, idx_pos + size_of::<VIdPos>());
+                (offset, (sr_pos, eqv_poses))
             })
         })
         .collect()
-}
-
-/// Groups the characteristics by neighbors' label.
-fn group_characteristics_by_nlabel<ID>(
-    ids: ID,
-    infos: &[CharacteristicInfo],
-) -> BTreeMap<VLabel, Vec<usize>>
-where
-    ID: IntoIterator<Item = usize>,
-{
-    let mut nlabel_ids: BTreeMap<VLabel, Vec<usize>> = BTreeMap::new();
-    ids.into_iter().for_each(|id| {
-        unsafe { infos.get_unchecked(id) }
-            .nlabel_ninfo_eqv()
-            .keys()
-            .for_each(|&nlabel| nlabel_ids.entry(nlabel).or_default().push(id))
-    });
-    nlabel_ids
 }
 
 /// Tries to allocate space for a SuperRow.
@@ -372,7 +365,7 @@ fn allocate(
     if let Some(_) = right {
         None
     } else {
-        let root_pos = *unsafe { eqv_poses.get_unchecked(0) };
+        let root_pos = eqv_poses[0];
         super_row_mm.resize(new_sr_pos);
         write_num_bytes(super_row_mm, sr_pos, new_sr_pos - sr_pos);
         write_pos_len(super_row_mm, sr_pos, 0, root_pos, 1);
@@ -383,22 +376,21 @@ fn allocate(
 
 /// Update the `SuperRowHeader` and truncate `super_row_mm` and `index_mm` in `ids`.
 fn finish_results(
-    ids: &[usize],
     infos: &[CharacteristicInfo],
     super_row_mms: &mut [MemoryManager],
     index_mms: &mut [MemoryManager],
     sr_pos_idx_poses: &[(usize, usize)],
 ) {
-    ids.iter().for_each(|&id| unsafe {
-        let &(sr_pos, idx_pos) = sr_pos_idx_poses.get_unchecked(id);
+    infos.iter().for_each(|info| {
+        let (sr_pos, idx_pos) = sr_pos_idx_poses[info.id()];
         write_super_row_header(
-            super_row_mms.get_unchecked_mut(id),
+            &mut super_row_mms[info.id()],
             idx_pos / size_of::<VIdPos>(),
-            infos.get_unchecked(id).characteristic().infos().len() + 1,
+            info.characteristic().infos().len() + 1,
             1,
         );
-        super_row_mms.get_unchecked_mut(id).resize(sr_pos);
-        index_mms.get_unchecked_mut(id).resize(idx_pos);
+        super_row_mms[info.id()].resize(sr_pos);
+        index_mms[info.id()].resize(idx_pos);
     });
 }
 
@@ -470,7 +462,7 @@ mod tests {
         let pattern_graph = create_pattern_graph();
         let infos = vec![
             CharacteristicInfo::new(Characteristic::new(&pattern_graph, 1), 0),
-            CharacteristicInfo::new(Characteristic::new(&pattern_graph, 5), 2),
+            CharacteristicInfo::new(Characteristic::new(&pattern_graph, 5), 1),
         ];
         let mut super_row_mms = vec![MemoryManager::Mem(vec![]), MemoryManager::Mem(vec![])];
         let mut index_mms = vec![MemoryManager::Mem(vec![]), MemoryManager::Mem(vec![])];
@@ -529,5 +521,104 @@ mod tests {
                 .map(|mm| mm.read_slice::<u8>(0, mm.len()))
                 .collect::<Vec<_>>()
         );
+    }
+
+    fn create_data_graph_q02() -> DataGraph {
+        let vertices = vec![(1, 1), (2, 2), (3, 3), (4, 4), (11, 1), (14, 4)];
+        let arcs = vec![(1, 2, 0), (2, 3, 0), (3, 4, 0), (3, 14, 0), (11, 2, 0)];
+        let mut mm = MemoryManager::Mem(vec![]);
+        mm_read_iter(
+            &mut mm,
+            vertices
+                .iter()
+                .map(|&(_, l)| l)
+                .collect::<HashSet<_>>()
+                .len(),
+            vertices.len(),
+            arcs.len(),
+            vertices,
+            arcs,
+        );
+        DataGraph::new(mm)
+    }
+
+    fn create_pattern_graph_q02<'a>() -> PatternGraph<'a> {
+        let mut pattern = PatternGraph::new();
+        vec![(1, 1), (2, 2), (3, 3), (4, 4)]
+            .into_iter()
+            .for_each(|(v, l)| pattern.add_vertex(v, l));
+        vec![(1, 2, 0), (2, 3, 0), (3, 4, 0)]
+            .into_iter()
+            .for_each(|(u1, u2, l)| {
+                pattern.add_arc(u1, u2, l);
+            });
+        pattern
+    }
+
+    fn assert_mm_eq(mms1: &[MemoryManager], mms2: &[MemoryManager]) {
+        assert_eq!(
+            mms1.iter()
+                .map(|mm| mm.read_slice::<u8>(0, mm.len()))
+                .collect::<Vec<_>>(),
+            mms2.iter()
+                .map(|mm| mm.read_slice::<u8>(0, mm.len()))
+                .collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn test_q02() {
+        let data_graph = create_data_graph_q02();
+        let pattern_graph = create_pattern_graph_q02();
+        let mut super_row_mms = vec![
+            MemoryManager::Mem(vec![]),
+            MemoryManager::Mem(vec![]),
+            MemoryManager::Mem(vec![]),
+        ];
+        let mut index_mms = vec![MemoryManager::Mem(vec![]), MemoryManager::Mem(vec![])];
+        let mut sr_mms = vec![
+            MemoryManager::Mem(vec![]),
+            MemoryManager::Mem(vec![]),
+            MemoryManager::Mem(vec![]),
+        ];
+        let mut idx_mms = vec![MemoryManager::Mem(vec![]), MemoryManager::Mem(vec![])];
+        match_characteristics(
+            &data_graph,
+            2,
+            &[CharacteristicInfo::new(
+                Characteristic::new(&pattern_graph, 2),
+                0,
+            )],
+            &mut super_row_mms,
+            &mut index_mms,
+        );
+        empty_super_row_mm(&mut sr_mms[0], 3, 1);
+        add_super_row_and_index(
+            &mut sr_mms[0],
+            &mut idx_mms[0],
+            &[1, 2, 1],
+            &[&[2], &[1, 11], &[3]],
+        );
+        assert_mm_eq(&super_row_mms, &sr_mms);
+        assert_mm_eq(&index_mms, &idx_mms);
+        match_characteristics(
+            &data_graph,
+            3,
+            &[CharacteristicInfo::new(
+                Characteristic::new(&pattern_graph, 3),
+                1,
+            )],
+            &mut super_row_mms,
+            &mut index_mms,
+        );
+        empty_super_row_mm(&mut sr_mms[1], 3, 1);
+        add_super_row_and_index(
+            &mut sr_mms[1],
+            &mut idx_mms[1],
+            &[1, 1, 2],
+            &[&[3], &[2], &[4, 14]],
+        );
+        assert_mm_eq(&super_row_mms, &sr_mms);
+        assert_mm_eq(&index_mms, &idx_mms);
     }
 }
---
+++ tests/q02.rs
@@ -0,0 +1,60 @@
+use opgm::{
+    compiler::compiler::compile,
+    data_graph::{mm_read_iter, DataGraph},
+    memory_manager::MemoryManager,
+    planner::Task,
+};
+use std::collections::HashSet;
+
+const QUERY: &str = "\
+(match (vertices (u1 1) (u2 2) (u3 3) (u4 4))
+       (arcs (u1 u2 0) (u2 u3 0) (u3 u4 0)))";
+
+fn create_data_graph() -> DataGraph {
+    let vertices = vec![(1, 1), (2, 2), (3, 3), (4, 4), (11, 1), (14, 4)];
+    let arcs = vec![(1, 2, 0), (2, 3, 0), (3, 4, 0), (3, 14, 0), (11, 2, 0)];
+    let mut mm = MemoryManager::Mem(vec![]);
+    mm_read_iter(
+        &mut mm,
+        vertices
+            .iter()
+            .map(|&(_, l)| l)
+            .collect::<HashSet<_>>()
+            .len(),
+        vertices.len(),
+        arcs.len(),
+        vertices,
+        arcs,
+    );
+    DataGraph::new(mm)
+}
+
+#[test]
+fn test_match() {
+    let data_graph = create_data_graph();
+    let (pattern_graph, vc_info, ec_info, gcs) = compile(QUERY).unwrap();
+    let mut task = Task::new(&data_graph, pattern_graph, &vc_info, &ec_info, gcs);
+    let plan = task.prepare().plan();
+    assert_eq!(
+        plan.stars()
+            .iter()
+            .map(|star| star.root())
+            .collect::<Vec<_>>(),
+        vec![2, 3]
+    );
+    let (mut super_row_mms, mut index_mms) = plan.allocate();
+    assert_eq!(super_row_mms.len(), 3);
+    assert_eq!(index_mms.len(), 2);
+    let mut result = vec![];
+    plan.execute(&mut result, &mut super_row_mms, &mut index_mms)
+        .unwrap();
+    assert_eq!(
+        std::str::from_utf8(&result).unwrap(),
+        "u1,u2,u3,u4
+1,2,3,4
+1,2,3,14
+11,2,3,4
+11,2,3,14
+"
+    );
+}
