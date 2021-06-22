1acd5458caf7b04c7699e860de1980e0e96ec83c
E-Neo <e-neo@qq.com>
2021-06-22 22:14:31 +0800

    new methods for memory manager

--- src/memory_manager/memory_manager.rs
+++ src/memory_manager/memory_manager.rs
@@ -5,18 +5,16 @@ use std::path::Path;
 /// A read-only memory mapped file.
 pub struct MmapFile {
     mmap: Mmap,
-    len: u64,
 }
 
 impl MmapFile {
     pub fn from_file(file: &File) -> Self {
-        let len = file.metadata().unwrap().len();
         let mmap = unsafe { Mmap::map(&file) }.unwrap();
-        Self { mmap, len }
+        Self { mmap }
     }
 
     pub fn len(&self) -> usize {
-        self.len as usize
+        self.mmap.len()
     }
 
     pub fn read<T>(&self, pos: usize) -> *const T {
@@ -95,21 +93,59 @@ pub enum MemoryManager {
 }
 
 impl MemoryManager {
+    pub fn new_mem(size: usize) -> Self {
+        MemoryManager::Mem(vec![0; size])
+    }
+
+    pub fn new_mmap<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
+        Ok(MemoryManager::Mmap(MmapFile {
+            mmap: unsafe { Mmap::map(&File::open(path)?)? },
+        }))
+    }
+
+    pub fn new_mmap_mut<P: AsRef<Path>>(path: P, size: usize) -> std::io::Result<Self> {
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(path)?;
+        file.set_len(size as u64)?;
+        Ok(MemoryManager::MmapMut(MmapMutFile::from_file(file)))
+    }
+
+    pub fn new_sink() -> Self {
+        MemoryManager::Sink
+    }
+
     pub fn len(&self) -> usize {
         match self {
             MemoryManager::Mem(vec) => vec.len(),
             MemoryManager::Mmap(mmapfile) => mmapfile.len(),
             MemoryManager::MmapMut(mmapfile) => mmapfile.len(),
-            MemoryManager::Sink => 0,
+            MemoryManager::Sink => usize::MAX,
         }
     }
 
     pub fn resize(&mut self, new_len: usize) {
         match self {
             MemoryManager::Mem(vec) => vec.resize(new_len, 0),
-            MemoryManager::Mmap(_) => panic!("Cannot resize read only file"),
+            MemoryManager::Mmap(_) => unimplemented!(),
             MemoryManager::MmapMut(mmapfile) => mmapfile.resize(new_len),
             MemoryManager::Sink => (),
+        }
+    }
+
+    pub unsafe fn as_ref<T>(&self, pos: usize) -> &T {
+        match self {
+            MemoryManager::Mem(vec) => (vec.as_ptr().add(pos) as *const T).as_ref().unwrap(),
+            MemoryManager::Mmap(mmapfile) => (mmapfile.mmap.as_ptr().add(pos) as *const T)
+                .as_ref()
+                .unwrap(),
+            MemoryManager::MmapMut(mmapfile) => (mmapfile.mmap.as_ptr().add(pos) as *const T)
+                .as_ref()
+                .unwrap(),
+            MemoryManager::Sink => unimplemented!(),
         }
     }
 
@@ -138,6 +174,21 @@ impl MemoryManager {
         }
     }
 
+    pub unsafe fn copy_from_slice<T>(&mut self, pos: usize, src: &[T]) {
+        match self {
+            MemoryManager::Mem(vec) => {
+                std::ptr::copy(src.as_ptr(), vec.as_mut_ptr().add(pos) as *mut T, src.len())
+            }
+            MemoryManager::Mmap(_) => unimplemented!(),
+            MemoryManager::MmapMut(mmapfile) => std::ptr::copy(
+                src.as_ptr(),
+                mmapfile.mmap.as_mut_ptr().add(pos) as *mut T,
+                src.len(),
+            ),
+            MemoryManager::Sink => (),
+        }
+    }
+
     pub fn read<T>(&self, pos: usize) -> *const T {
         match self {
             MemoryManager::Mem(vec) => unsafe { vec.as_ptr().add(pos) as *const T },
@@ -161,6 +212,8 @@ impl MemoryManager {
 
 #[cfg(test)]
 mod tests {
+    use tempfile::NamedTempFile;
+
     use super::*;
     use std::{io::Write, mem::size_of};
 
@@ -276,9 +329,62 @@ mod tests {
     }
 
     #[test]
+    fn test_mem() {
+        let mut mm = MemoryManager::new_mem(29);
+        assert_eq!(mm.len(), 29);
+        unsafe {
+            mm.copy_from_slice::<i32>(1, &[3, 2, 1]);
+        }
+        assert_eq!(unsafe { mm.as_ref::<i32>(1) }, &3);
+        assert_eq!(unsafe { mm.as_slice::<i32>(1, 3) }, &[3, 2, 1]);
+        unsafe { mm.as_mut_slice::<i32>(1, 3) }.sort();
+        assert_eq!(unsafe { mm.as_slice::<i32>(1, 3) }, &[1, 2, 3]);
+        mm.resize(3 * size_of::<i32>());
+        assert_eq!(unsafe { mm.as_slice::<i32>(1, 3) }, &[1, 2, 3]);
+    }
+
+    #[test]
+    fn test_mmap() {
+        let mut file = NamedTempFile::new().unwrap();
+        let data: &[i32] = &[3, 2, 1];
+        file.write_all(unsafe {
+            std::slice::from_raw_parts(data.as_ptr() as *const u8, size_of::<i32>() * data.len())
+        })
+        .unwrap();
+        let path = file.into_temp_path();
+        let mm = MemoryManager::new_mmap(path).unwrap();
+        assert_eq!(mm.len(), 3 * size_of::<i32>());
+        assert_eq!(unsafe { mm.as_ref::<i32>(0) }, &3);
+        assert_eq!(unsafe { mm.as_slice::<i32>(0, 3) }, &[3, 2, 1]);
+    }
+
+    #[test]
+    fn test_mmap_mut() {
+        let mut file = NamedTempFile::new().unwrap();
+        let data: &[i32] = &[3, 2, 1];
+        file.write_all(unsafe {
+            std::slice::from_raw_parts(data.as_ptr() as *const u8, size_of::<i32>() * data.len())
+        })
+        .unwrap();
+        let path = file.into_temp_path();
+        let mut mm = MemoryManager::new_mmap_mut(path, 29).unwrap();
+        assert_eq!(mm.len(), 29);
+        assert_eq!(unsafe { mm.as_slice::<i32>(0, mm.len()) }, &[0; 29]);
+        unsafe {
+            mm.copy_from_slice::<i32>(1, &[3, 2, 1]);
+        }
+        assert_eq!(unsafe { mm.as_ref::<i32>(1) }, &3);
+        assert_eq!(unsafe { mm.as_slice::<i32>(1, 3) }, &[3, 2, 1]);
+        unsafe { mm.as_mut_slice::<i32>(1, 3) }.sort();
+        assert_eq!(unsafe { mm.as_slice::<i32>(1, 3) }, &[1, 2, 3]);
+        mm.resize(3 * size_of::<i32>());
+        assert_eq!(unsafe { mm.as_slice::<i32>(1, 3) }, &[1, 2, 3]);
+    }
+
+    #[test]
     fn test_sink() {
         let mut mm = MemoryManager::Sink;
-        assert_eq!(mm.len(), 0);
+        assert_eq!(mm.len(), usize::MAX);
         let data = [1, 2, 3, 4, 5, 6];
         mm.write(99999, data.as_ptr(), data.len());
         mm.resize(1000);
